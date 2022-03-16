a04ec1703e86ceb9511ae0321c4d5b9b73b1365a
cecilia-wanja <cclwanja.cw@gmail.com>
2022-03-16 05:24:43 +0300

    prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,42 +12,55 @@ int _putchar(char c);
 
 void print_times_table(int n)
 {
-	int i, j, res;
+	int i;
+	int j;
 
-	if (!(n > 15 || n < 0))
+	if (n >= 0 && n <= 15)
 	{
+
 		for (i = 0; i <= n; i++)
 		{
+
 			for (j = 0; j <= n; j++)
 			{
-				res = (i * j);
-				if (j != 0)
+
+				if ((i * j) < 10)
 				{
-					_putchar(',');
-					_putchar(' ');
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar((j * i) + '0');
 				}
-				if (res < 10 && j != 0)
+
+				else if ((i * j) < 100)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((res % 10) + '0');
+					_putchar((((j * i) / 10) % 10) + '0');
+					_putchar(((j * i) % 10) + '0');
 				}
-				else if (res >= 10 && res < 100)
+
+				else if ((i * j) < 1000)
 				{
 					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
+					_putchar((((j * i) / 100) % 10) + '0');
+					_putchar((((j * i) / 10) % 10) + '0');
+					_putchar(((j * i) % 10) + '0');
 				}
-				else if (res >= 100 && j != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
-				}
-				else
-					_putchar((res % 10) + '0');
+			if (j != (n))
+			{
+				_putchar(',');
 			}
-			_putchar('\n');
+
+			}
+
+		_putchar('\n');
+
 		}
+
 	}
+
 }
