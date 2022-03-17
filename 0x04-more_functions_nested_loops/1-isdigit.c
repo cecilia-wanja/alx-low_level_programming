6b71d1ba6f18b24d7f2a666e4f00c0a16bb10590
cecilia-wanja <cclwanja.cw@gmail.com>
2022-03-17 20:28:09 +0300

    checks for a digit (0 through 9)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-
+/**
+ * _isdigit - Check if paramter c represents an digit.
+ * @c: character to check
+ * Return: Return 1 if it is a digit otherwise return 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
