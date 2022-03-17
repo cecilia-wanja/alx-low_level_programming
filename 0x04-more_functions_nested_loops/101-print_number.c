be53fc91e0a89b450c0b09e78bb36119d9ae9070
cecilia-wanja <cclwanja.cw@gmail.com>
2022-03-17 22:28:24 +0300

    prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,17 +1,9 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int _putchar(char c);
 
 /**
- * print_number - prints
- *
- * @n: integer character
- *
- * No return
+ * print_number - prints a number
+ * @n: Input number
  */
-
 
 void print_number(int n)
 {
