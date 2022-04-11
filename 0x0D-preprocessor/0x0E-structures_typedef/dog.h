25b7bcb2bb82ced347e17c56ae7e439c48b248da
cecilia-wanja <cclwanja.cw@gmail.com>
2022-04-11 22:05:09 +0300

    Define a new type struct dog

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -5,13 +5,12 @@
 #include <stdlib.h>
 
 /**
-*struct dog - a new data type
+*struct dog - a new type of data
 *representing a dog
 *@name: name of dog
 *@age: age of dog
 *@owner: owner's name
 */
-
 struct dog
 {
 char *name;
