401cc7c252581c0ccc4735bfdeaefaa4848b1525
cecilia-wanja <cclwanja.cw@gmail.com>
2022-07-01 21:33:31 +0300

    Creating a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,9 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - creates a hash table
  * @size: size of the table
  * Return: hash table, otherwise  NULL
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
